3bf50a45ea93849f2016caeb56e5c47e3ebf4a2c
Filip Wierzbinka <fwierzbinka@student.agh.edu.pl>
2022-09-04 20:28:27 +0200

    ball moving update

--- rtl/draw_ball_ctl.v
+++ rtl/draw_ball_ctl.v
@@ -17,17 +17,17 @@ module draw_ball_ctl
     
     always@*
     begin
-        if( xpos_player_1 + PLAYERS_RADIUS == xpos_ball - RADIUS_BALL )
-            xpos_ball_nxt = xpos_ball + 1;
-        else if ( xpos_player_1 - PLAYERS_RADIUS == xpos_ball + RADIUS_BALL )
-            xpos_ball_nxt = xpos_ball - 1;
+        if( ( xpos_player_1 + PLAYERS_RADIUS <= xpos_ball - RADIUS_BALL ) && ( xpos_player_1 + PLAYERS_RADIUS >= xpos_ball - RADIUS_BALL - 2) )
+            xpos_ball_nxt = xpos_ball + 2;
+        else if ( ( xpos_player_1 - PLAYERS_RADIUS >= xpos_ball + RADIUS_BALL ) && ( xpos_player_1 - PLAYERS_RADIUS <= xpos_ball + RADIUS_BALL + 2) )
+            xpos_ball_nxt = xpos_ball - 2;
         else
             xpos_ball_nxt = xpos_ball;
         
-        if( ypos_player_1 - PLAYERS_RADIUS == ypos_ball + RADIUS_BALL )
-            ypos_ball_nxt = ypos_ball - 1;
-        else if( ypos_player_1 + PLAYERS_RADIUS == ypos_ball - RADIUS_BALL )
-            ypos_ball_nxt = ypos_ball + 1;
+        if( ( ypos_player_1 - PLAYERS_RADIUS >= ypos_ball + RADIUS_BALL ) && ( ypos_player_1 - PLAYERS_RADIUS <= ypos_ball + RADIUS_BALL + 2) )
+            ypos_ball_nxt = ypos_ball - 2;
+        else if( ( ypos_player_1 + PLAYERS_RADIUS <= ypos_ball - RADIUS_BALL ) && ( ypos_player_1 + PLAYERS_RADIUS >= ypos_ball - RADIUS_BALL - 2 ) )
+            ypos_ball_nxt = ypos_ball + 2;
         else
             ypos_ball_nxt = ypos_ball;
     end
